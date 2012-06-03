1f418210442f2e64b1422aabe9d6dc7cf74db444
Artem Falcon <artem.falcon@gmail.com>
2012-06-03 17:19:09 +0400

    Cleanup.

--- ps2_synaptics/patch-ps2_synaptics.cpp
+++ ps2_synaptics/patch-ps2_synaptics.cpp
@@ -1,5 +1,5 @@
---- /boot/home/src/src/add-ons/kernel/bus_managers/ps2/ps2_synaptics.cpp.orig	2012-05-31 11:43:35.539492352 +0400
-+++ /boot/home/src/src/add-ons/kernel/bus_managers/ps2/ps2_synaptics.cpp	2012-05-31 11:47:54.794034176 +0400
+--- ps2_synaptics.cpp.orig	2012-05-31 11:43:35.539492352 +0400
++++ ps2_synaptics.cpp	2012-06-03 17:07:26.320077824 +0400
 @@ -29,6 +29,13 @@
  #define REAL_MAX_PRESSURE		100
  #define MAX_PRESSURE			200
@@ -19,7 +19,7 @@
  	int8 i;
  	uint8 val[8];
 +
-+	ps2_dev_command_timeout(dev, 0xE6, NULL, 0, NULL, 0, 1000000);
++	ps2_dev_command_timeout(dev, 0xE6, NULL, 0, NULL, 0, 500000);
  	for (i = 0; i < 4; i++) {
  		val[2 * i] = (arg >> (6 - 2 * i)) & 3;
  		val[2 * i + 1] = 0xE8;
@@ -32,27 +32,20 @@
  	status_t statusOfEnable = ps2_dev_command(dev->parent_dev, PS2_CMD_ENABLE,
  			NULL, 0, NULL, 0);
  	if (statusOfEnable != B_OK)
-@@ -278,7 +287,18 @@ probe_synaptics(ps2_dev *dev)
+@@ -278,7 +287,11 @@ probe_synaptics(ps2_dev *dev)
  	uint8 val[3];
  	uint8 deviceId;
  	status_t status;
 -	TRACE("SYNAPTICS: probe\n");
-+	bigtime_t timeout = 400000, start;
 +
 +	dprintf("SYNAPTICS: probe\n");
 +
-+	// required delay
-+	start = system_time();
-+	while (1) {
-+		if ((system_time() - start) > timeout) {
-+			break;
-+		}
-+		snooze(timeout);
-+	}
++	// Required delay
++	snooze(400000);
  
  	status = send_touchpad_arg(dev, 0x00);
  	if (status != B_OK)
-@@ -294,14 +314,14 @@ probe_synaptics(ps2_dev *dev)
+@@ -294,14 +307,14 @@ probe_synaptics(ps2_dev *dev)
  		return B_ERROR;
  	}
  
