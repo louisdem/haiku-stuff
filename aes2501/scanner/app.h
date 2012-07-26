d441f0903812cd29cf9c87163505ae2f377fd64e
Artem Falcon <artem.falcon@gmail.com>
2012-07-26 19:13:23 +0400

    Start 'Run' in separate thread.

--- aes2501/scanner/app.h
+++ aes2501/scanner/app.h
@@ -1,22 +1,30 @@
 #ifndef AESCAN_H
 #define AESCAN_H
 
+// (bind-opaque-type thread_func (function integer32 (c-pointer)))
+typedef long (*thread_func)(void *);
+
 ___abstract class Proto
 {
 public:
 virtual void MessageReceived(BMessage*) = 0;
-virtual void ReadyToRun() { be_app->PostMessage(B_QUIT_REQUESTED); };
+virtual void ReadyToRun() { be_app->PostMessage(B_QUIT_REQUESTED); }
 };
 class AEScan: public BApplication
 {
 public:
-AEScan(const char *sig) : BApplication(sig) {};
-
-void _Run() { Run(); };
-AEScan *DoQuit() { 
-	be_app_messenger.SendMessage(B_QUIT_REQUESTED);
-	return this;
+AEScan(const char *sig) : BApplication(sig) {}
+~AEScan() { be_app_messenger.SendMessage(B_QUIT_REQUESTED); }
 };
+
+// !
+static long RunProxy(void *_this) {
+        AEScan *scanner = (AEScan *) _this;
+        scanner->Lock();
+        return scanner->Run();
+}
+static long SpawnThreadProxy(const char *s, long p, AEScan *o) {
+        return spawn_thread(RunProxy /* ! */, s, p, o);
 };
 
 #endif
--- aes2501/scanner/main.scm
+++ aes2501/scanner/main.scm
@@ -1,14 +1,26 @@
 (require-extension bind coops cplusplus-object)
-(bind* "#include <Application.h>\n#include \"app.h\"\n#include \"common\/aes2501_app_common.h\"")
+(bind* "#include <Application.h>\n#include <kernel/OS.h>\n"
+ "#include \"app.h\"\n#include \"common/aes2501_app_common.h\"")
+(define resume_thread (foreign-lambda integer32 "resume_thread" integer32))
+#|(define spawn_thread (foreign-lambda integer32 "spawn_thread" thread_func
+ (const c-string) integer32 c-pointer))|#
+(bind "___s32 spawn_thread(thread_func, const char *, ___s32, AEScan *);") ; !
+
 ; (define instance (force init-val))
 (define init-val (delay
  (let ((obj (new <AEScan> kAesSignature)))
-  (_Run obj)
+  (kernel-thread-start! (make-kernel-thread obj))
   obj
  )
 ))
-(define (do-exit obj)
- (delete (DoQuit obj))
+(define (b-good? ch) (if (>= ch 0) #t #f))
+(define (kernel-thread-start! th)
+ (if (b-good? th)
+  (resume_thread th)
+ )
+)
+(define (make-kernel-thread obj)
+ (SpawnThreadProxy "AEScan Runner" 50 obj)
 )
 
 ;;;
